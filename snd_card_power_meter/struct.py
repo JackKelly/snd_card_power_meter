a8de515a2ea07bc4cb72b05aaf18b8502e15dfb3
Jack Kelly <jack-list@xlk.org.uk>
2013-03-08 09:59:29 +0000

    adding note about where I got the Struct code from

--- snd_card_power_meter/struct.py
+++ snd_card_power_meter/struct.py
@@ -1,3 +1,5 @@
 class Struct(object):
+    # Adapted from 
+    # http://stackoverflow.com/questions/35988/c-like-structures-in-python
     def __init__(self, **kwds):
         self.__dict__.update(kwds)
