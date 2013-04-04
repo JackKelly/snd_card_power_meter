10937200d5ef0f8f24fccca215a635bafb0092a8
Jack Kelly <jack-list@xlk.org.uk>
2013-04-04 13:22:52 +0100

    don't process file that's currently being updated

--- scripts/convert_all.py
+++ scripts/convert_all.py
@@ -5,12 +5,28 @@ Converts all remaining WAV files to FLAC
 """
 
 from __future__ import print_function, division
-import subprocess, os
+import subprocess, os, time
 import snd_card_power_meter.config as config
 
 def main():
     all_files = os.listdir(config.FLAC_DIR)
     wav_files = [f for f in all_files if f.rpartition('.')[-1]=='wav']
+    
+    if not wav_files:
+        print("No wav files in", config.FLAC_DIR)
+        return
+    
+    # check if the last file is currently being updated.  if so then we
+    # mustn't touch it.
+    wav_files.sort()
+    last_wavfile = config.FLAC_DIR + "/" + wav_files[-1]
+    size1 = os.path.getsize(last_wavfile)
+    time.sleep(2)
+    size2 = os.path.getsize(last_wavfile)
+    if size2 > size1:
+        print("Not processing", last_wavfile, "because it was just updated.")
+        wav_files.pop()
+    
     wav_files_prefixes = [f.rpartition('.')[0] for f in wav_files]
     
     for f in wav_files_prefixes:
