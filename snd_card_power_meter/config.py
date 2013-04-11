3686d9176af700c3a3cb1840fe7d221a56244582
Jack Kelly <jack-list@xlk.org.uk>
2013-04-11 17:08:59 +0100

    Increase FRAMES_PER_BUFFER to 2048 as a candidate fix for #32, [Errno Input overflowed] -9981

--- snd_card_power_meter/config.py
+++ snd_card_power_meter/config.py
@@ -10,10 +10,10 @@ DOWNSAMPLED_RATE = 16000 # Hz (MIT REDD uses 15kHz but 16kHz is a standard
 #                              rate and so increases compatibility)
 RECORD_SECONDS = 1 # Seconds to record per queue item
 N_CHANNELS = 2 # one for voltage, one for current 
-FRAMES_PER_BUFFER = 1024
+FRAMES_PER_BUFFER = 2048
 SAMPLE_FORMAT = pyaudio.paInt32
 SAMPLE_WIDTH = pyaudio.get_sample_size(SAMPLE_FORMAT)
-N_READS_PER_QUEUE_ITEM = int(round((FRAME_RATE / FRAMES_PER_BUFFER) 
+N_READS_PER_QUEUE_ITEM = int(round(FRAME_RATE / FRAMES_PER_BUFFER
                                    * RECORD_SECONDS))
 
 ##########################################################################
